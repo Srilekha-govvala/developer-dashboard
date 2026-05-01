224057ee161d9a3a6fcea47906d5f8eb2d4a2ba0
Srilekha <srilekhagovvala441@gmail.com>
2026-05-01 19:55:33 +0530

    #Deploying project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Projects-Vault/developer-dashboard/',
+  base: '/developer-dashboard/',
   plugins: [react()],
 })
